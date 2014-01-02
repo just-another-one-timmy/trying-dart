b06da798e3fe445e2af3853919f2247bf2cb923d
Iaroslav Tymchenko <jtimchenko@gmail.com>
2014-01-03 01:47:32 +0900

    Server closes websocket after sending 10 messages.

--- web/webapp/main.dart
+++ web/webapp/main.dart
@@ -6,6 +6,9 @@ void log(msg) {
   print(msg);
 }
 
+// TODO: figure out how to handle situation when client closes websocket.
+// Currently server keeps sending messages somewhere o_O without any
+// exceptions.
 void handleWebSocket(WebSocket w) {
   log("Client connected.");
   clientsCount++;
@@ -13,11 +16,20 @@ void handleWebSocket(WebSocket w) {
   var currentMessage = 0;
   w.listen((e) {
     log("Client says ${e}");
+    // Logically this work's only when each client only says "Ping" once.
     new Timer.periodic(
         new Duration(seconds: 1),
         (t) {
           currentMessage++;
+          log("Sending a message to client.");
           w.add("Pong, you're my client #${currentClient} and this is my message #${currentMessage} to you.");
+          if (currentMessage >= 10) {
+            w.add("You've received too much attention from me. Bye-bye.");
+            w.close().then((_) {
+              log("Closed websocket for client #${currentClient}.");
+            });
+            t.cancel();
+          }
         });
   });
 }
