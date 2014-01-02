e376aec4f36563932dc064ebbc3ed91dbafb723e
Iaroslav Tymchenko <jtimchenko@gmail.com>
2014-01-03 01:11:17 +0900

    Using websockets.

--- web/client.dart
+++ web/client.dart
@@ -1,5 +1,20 @@
 import 'dart:html';
 
+void log(msg) {
+  document.querySelector("#log").append(new LIElement()..text=msg);
+}
+
 void main() {
   document.querySelector("#hello").innerHtml = "<b>Hi from Dart!</b>";
+  WebSocket ws = new WebSocket("ws://127.0.0.1:8080/ws");
+  ws.onOpen.listen((e) {
+    log("WebSocket opened.");
+    ws.send("Ping!");
+  });
+  ws.onClose.listen((e) {
+    log("WebSocket closed.");
+  });
+  ws.onMessage.listen((e) {
+    log("Server says: ${e.data}");
+  });
 }
--- web/webapp/main.dart
+++ web/webapp/main.dart
@@ -1,5 +1,31 @@
 import 'package:stream/stream.dart';
+import 'dart:io';
+import 'dart:async';
+
+void log(msg) {
+  print(msg);
+}
+
+void handleWebSocket(WebSocket w) {
+  log("Client connected.");
+  clientsCount++;
+  var currentClient = clientsCount;
+  var currentMessage = 0;
+  w.listen((e) {
+    log("Client says ${e}");
+    new Timer.periodic(
+        new Duration(seconds: 1),
+        (t) {
+          currentMessage++;
+          w.add("Pong, you're my client #${currentClient} and this is my message #${currentMessage} to you.");
+        });
+  });
+}
+
+int clientsCount = 0;
 
 void main() {
-  new StreamServer().start();
+  new StreamServer(uriMapping: {
+    "ws:/ws": handleWebSocket
+  }).start();
 }
